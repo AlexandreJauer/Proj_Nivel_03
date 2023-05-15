49ccc24674a741ad19d9cb6a0a6507c40e109f84
AlexandreJauer <alexandrejauer@hotmail.com>
2023-05-14 23:32:35 -0300

    1ºenvio

---
+++ livros-react/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import  LivroLista  from './LivroLista';
+
+
+function App() {
+  return (
+    <div>
+      <LivroLista/>
+    </div>
+  );
+}
+
+export default App;
---
+++ livros-react/src/LivroLista.js
@@ -0,0 +1,68 @@
+import { useState, useEffect } from 'react';
+import { ControleLivros } from './controle/ControleLivros';
+import { ControleEditora } from './controle/ControleEditora';
+
+
+// Instanciar controlador de livros e editoras
+const controleLivros = new ControleLivros();
+const controleEditora = new ControleEditora();
+
+// Componente auxiliar LinhaLivro
+function LinhaLivro(props) {
+  const { livro, excluir } = props;
+  const nomeEditora = controleEditora.getNomeEditora(livro.codEditora);
+
+  return (
+    <tr>
+      <td><button onClick={() => excluir(livro.codigo)}>Excluir</button></td>
+      <td>{livro.titulo}</td>
+      <td>{livro.autores.join(", ")}</td>
+      <td>{nomeEditora}</td>
+    </tr>
+  );
+}
+
+// Componente LivroLista
+export default function LivroLista() {
+  const [livros, setLivros] = useState([]);
+  const [carregado, setCarregado] = useState(false);
+
+  // Método para excluir livro
+  const excluir = (codigo) => {
+    controleLivros.excluir(codigo);
+    setCarregado(false);
+  }
+
+  // Buscar livros
+  useEffect(() => {
+    if (!carregado) {
+      setLivros(controleLivros.obterLivros());
+      setCarregado(true);
+    }
+  }, [carregado]);
+
+  return (
+    <main>
+      <h1>Lista de Livros</h1>
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>Título</th>
+            <th>Autor(es)</th>
+            <th>Editora</th>
+          </tr>
+        </thead>
+        <tbody>
+          {livros.map((livro) => (
+            <LinhaLivro
+              key={livro.codigo}
+              livro={livro}
+              excluir={excluir}
+            />
+          ))}
+        </tbody>
+      </table>
+    </main>
+  );
+}
